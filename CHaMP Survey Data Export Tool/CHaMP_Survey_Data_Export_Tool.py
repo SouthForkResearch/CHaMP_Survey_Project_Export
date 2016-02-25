5dd4cc9a417e2deb84fe4667dfbe9712167eb178
Kelly Whitehead <kelly@southforkresearch.org>
2016-02-25 13:39:47 -0800

    Initial Commit

---
+++ CHaMP Survey Data Export Tool/CHaMP_Survey_Data_Export_Tool.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+    Tool for converting a CHaMP SurveyGeodatabase into open-format GIS datasets.
+"""
+
+import sys
+import arcpy
+
+def main(strInputSurveyGDB,strOutputPath):
+
+
+    return
+
+if __name__ == "__main__":
+
+    main(sys.argv[1],sys.argv[2])
