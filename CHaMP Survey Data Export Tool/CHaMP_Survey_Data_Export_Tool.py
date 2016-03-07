73de06e0ec88c001b6e377180e70061f86aa653b
Kelly Whitehead <kelly@southforkresearch.org>
2016-03-07 12:42:52 -0800

    overwrite (delete, actually) all existing files in output path before copying files to output.

--- CHaMP Survey Data Export Tool/CHaMP_Survey_Data_Export_Tool.py
+++ CHaMP Survey Data Export Tool/CHaMP_Survey_Data_Export_Tool.py
@@ -3,24 +3,40 @@
 """
     Tool for converting a CHaMP SurveyGeodatabase into open-format GIS datasets.
 """
-
+import os 
+import shutil
 import sys
+import time
 import arcpy
 import CHaMP_Data
 
+
 def main(strInputSurveyGDB,strOutputPath):
+    
+    print "Starting CHaMP Survey Export Tool at " + str(time.asctime())
+    print "Input SurveyGDB: " + str(strInputSurveyGDB)
+    print "Output Path: " + str(strOutputPath)
+
     reload(CHaMP_Data)
     SurveyGDB = CHaMP_Data.SurveyGeodatabase(strInputSurveyGDB)
     
     listTables = []
-    #listFiles = []
+   
 
     ## OutputWorkspace Prep
-    #arcpy.env.Workspace = strOutputPath
-    #for dataset in arcpy.ListDatasets():
-    #    if arcpy.Exists(dataset):
-    #        arcpy.Delete_management(dataset)
-    #        print "Deleted Existing File: " + str(dataset)
+    ### http://stackoverflow.com/questions/185936/delete-folder-contents-in-python ###
+    print "Checking output directory..."
+    for file in os.listdir(strOutputPath):
+        file_path = os.path.join(strOutputPath, file)
+        try:
+            if os.path.isfile(file_path):
+                print "Deleting existing file: " + str(file_path)
+                os.unlink(file_path)
+            elif os.path.isdir(file_path): 
+                shutil.rmtree(file_path)
+                print "Deleting existing directory: " + str(file_path)
+        except Exception, e:
+            print e
     ### Write to Log File
 
     ## Rasters
@@ -48,7 +64,7 @@ def main(strInputSurveyGDB,strOutputPath):
         else:
             print str(table.filename) + " does not exist."
 
-    print "Export Complete."        
+    print "Export Complete  at " + str(time.asctime())        
             
     return
 
